51ab75b6ee20de66b5ce644d0a9a8bae5d963998
HackathonTemporary <hackathontemporary123@gmail.com>
2025-07-31 00:16:21 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ client.on(`messageCreate`,async (message)=>{
         {
             return message.reply("No Miku images available!");
         }
+      await message.reply("✅ Miku command received.");
         const url=images[Math.floor(Math.random()*images.length)];
         const embed=new EmbedBuilder().setTitle('🎀 Miku Miku oo ee oo 🎀')
         .setImage(url)
